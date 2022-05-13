7d59a415edcff844a11755fb66f5dd8bf2fec19f
chipbaber <chipbaber@yahoo.com>
2022-05-13 07:43:28 -0400

    updates to scripts

--- sendAPEXEmail.sql
+++ sendAPEXEmail.sql
@@ -4,22 +4,26 @@ l_email_code clob;
 l_email_text clob := 'Please enable HTML Email to view this content';
 gitLocation varchar2(2000) := 'https://raw.githubusercontent.com/chipbaber/apex_email/main/sample_html_email.html';
 email_id NUMBER;
+no_email_html EXCEPTION;
+PRAGMA EXCEPTION_INIT( no_email_html, -20001 );
 
 begin
 --Get the code from github
 l_email_code := apex_web_service.make_rest_request( p_url => gitLocation, p_http_method => 'GET');
 
+--Check to make sure we have the template Code if not raise a custom error
+if (DBMS_LOB.GETLENGTH(l_email_code) is null or DBMS_LOB.GETLENGTH(l_email_code) < 100) THEN
+ raise_application_error( -20001, 'No email sent. Unable to retrieve email template from external source.' );
+end if;
+
 --Replacement Text procedures title and text
 IF (:P8_CANDIDATE_TITLE is not null) THEN
-  l_email_code := replace(l_email_code,'Add Candidate Title',:P8_CANDIDATE_TITLE);
+  l_email_code := replace(l_email_code,'#Add Candidate Title#',:P8_CANDIDATE_TITLE);
 end if;
 
 IF (:P8_CANDIDATE_OVERVIEW is not null) THEN
-  l_email_code := replace(l_email_code,'Add Candidate Description',:P8_CANDIDATE_OVERVIEW);
+  l_email_code := replace(l_email_code,'#Add Candidate Description#',:P8_CANDIDATE_OVERVIEW);
 end if;
-
---Remove characters from base64 that cause issues in image display
-l_email_code := (APEX_UTIL.URL_ENCODE(l_email_code));
 
 --Build Email body to send, if checks in place
 IF (:P8_TO_EMAIL is not null) THEN
@@ -32,7 +36,7 @@ IF (:P8_TO_EMAIL is not null) THEN
         p_subj      => :P8_EMAIL_SUBJECT,
         p_cc        => NULL,
         p_bcc       => NULL,
-        p_replyto   => 'chipbaber@gmail.com');
+        p_replyto   => :P8_TO_REPLY_TO);
 
 -- Check for attachment, if present attach
  IF (:P8_CANDIDATE_RESUME is not null) THEN
@@ -53,5 +57,10 @@ apex_mail.push_queue();
 
   --do nothing
 end if;
+
+EXCEPTION
+when no_email_html
+then
+:P8_RETURN_ACTION := sqlerrm;
 
 end;
